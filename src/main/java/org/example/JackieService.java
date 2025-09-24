34dd0ce47d25523fa0fb52f721a7fbd25c668d9a
SagiBeno <benosagi321@gmail.com>
2025-09-24 11:51:41 +0200

    Solve task 2.

--- src/main/java/org/example/JackieService.java
+++ src/main/java/org/example/JackieService.java
@@ -23,17 +23,36 @@ public class JackieService {
         // count lines
         int countLines = countLines(filename);
         // init matrix
+        setMatrix(new int[countLines][6]);
         // read from file to matrix
+        readFromFile(filename);
     }
 
     private int countLines(String filename) throws FileNotFoundException {
         File fbe = new File(filename);
         Scanner scanner = new Scanner(fbe);
-        int result = 0;
+        scanner.nextLine();
+        int result = 0; // vagy result = 0-1
         while (scanner.hasNextLine()) {
             scanner.nextLine();
             result++;
         }
         return result;
+    }
+
+    private void readFromFile(String filename) throws FileNotFoundException {
+        File fbe = new File(filename);
+        Scanner scanner = new Scanner(fbe);
+        scanner.nextLine();
+        int index = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] split = line.split("\t");
+
+            for (int i = 0; i < 6; i++) {
+                matrix[index][i] = Integer.parseInt(split[i]);
+            }
+            index++;
+        }
     }
 }
